d5a3db9ba275fa8ecb04eae12003d2e56efb7236
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-05-27 13:23:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("D:/New folder/gcpdevopstest-424606-4e07e34ec952.json")
+ # credentials = file("D:/New folder/gcpdevopstest-424606-4e07e34ec952.json")
   project     = "gcpdevopstest-424606"
   region      = "us-central1"
 }
