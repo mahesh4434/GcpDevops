c2a2f0ac9ce72337ba8ca42b7e7021a7da7dff98
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-06-25 19:59:03 +0530

    Update display_recommendations.py

--- display_recommendations.py
+++ display_recommendations.py
@@ -3,18 +3,13 @@ import jenkins
 import pickle
 
 # Load the model and important features
-try:
-    with open('model.pkl', 'rb') as f:
-        model = pickle.load(f)
-    important_features = pd.read_csv('important_features.csv')
-except FileNotFoundError as e:
-    print(f"Error loading model or important features: {str(e)}")
-    model = None
-    important_features = pd.DataFrame()
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+important_features = pd.read_csv('important_features.csv')
 
 # Connect to Jenkins
-server = jenkins.Jenkins('http://localhost:8080/', username='mahesh4434', password='Dada@7744')
-job_name = 'GCP_Apply'
+server = jenkins.Jenkins('http://jenkins_url', username='user', password='password')
+job_name = 'job_name'
 
 def get_recommendations(build_number):
     # Example function to get recommendations based on build number
