572cb60d96814b4777e7c2fab2ebb22fdb779f74
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-06-25 18:57:47 +0530

    Update export_jenkins_data.py

--- export_jenkins_data.py
+++ export_jenkins_data.py
@@ -1,13 +1,25 @@
 import jenkins
 import json
 
-server = jenkins.Jenkins('http://jenkins_url', username='user', password='password')
-job_info = server.get_job_info('job_name')
+# Jenkins server details
+jenkins_url = 'http://localhost:8080/'
+username = 'mahesh4434'
+password = 'Dada@7744'
+job_name = 'GCP_Apply'
+
+# Connect to Jenkins server
+server = jenkins.Jenkins(jenkins_url, username=username, password=password)
+
+# Get information about the job
+job_info = server.get_job_info(job_name)
+
+# Get the list of builds for the job
 builds = job_info['builds']
 
+# Collect build information
 data = []
 for build in builds:
-    build_info = server.get_build_info('job_name', build['number'])
+    build_info = server.get_build_info(job_name, build['number'])
     data.append({
         'number': build['number'],
         'result': build_info['result'],
@@ -15,5 +27,9 @@ for build in builds:
         'timestamp': build_info['timestamp']
     })
 
-with open('jenkins_build_data.json', 'w') as f:
+# Save build data to a JSON file
+output_file = 'jenkins_build_data.json'
+with open(output_file, 'w') as f:
     json.dump(data, f)
+
+print(f'Build data exported to {output_file}')
