ccbfb747e3e397df6f60470a013d833f508ac467
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-06-27 16:13:58 +0530

    Update display_recommendations.py

--- display_recommendations.py
+++ display_recommendations.py
@@ -1,4 +1,4 @@
-import jenkins
+import json
 import pickle
 import pandas as pd
 from flask import Flask, render_template_string
@@ -9,62 +9,20 @@ with open(r'D:\New folder\POC\model.pkl', 'rb') as f:
 
 important_features = pd.read_csv(r'D:\New folder\POC\important_features.csv')
 
-# Connect to Jenkins
-server = jenkins.Jenkins('http://localhost:8080/', username='mahesh4434', password='Dada@7744')
-job_name = 'GCP_Apply'
-
-def get_recommendations(build_number):
-    # Example function to get recommendations based on build number
-    # Use your trained model to generate recommendations
-    # This is a placeholder function
-    # Replace this with your actual logic to generate recommendations
-    # For example, use the model to predict based on important features
-    # Here, we are assuming a placeholder recommendation.
-    return 'Optimize resource allocation for step X.'
-
-def get_build_data(job_name):
-    job_info = server.get_job_info(job_name)
-    builds = job_info['builds']
-
-    data = []
-    for build in builds:
-        build_info = server.get_build_info(job_name, build['number'])
-        data.append({
-            'number': build['number'],
-            'result': build_info['result'],
-            'duration': build_info['duration'],
-            'timestamp': build_info['timestamp']
-        })
-    return data
-
-def get_pipeline_data(job_name, build_number):
-    build_info = server.get_build_info(job_name, build_number)
-    pipeline_data = []
-    for action in build_info['actions']:
-        if 'stages' in action:
-            stages = action['stages']
-            for stage in stages:
-                pipeline_data.append({
-                    'name': stage['name'],
-                    'status': stage['status'],
-                    'duration': stage['durationMillis'],
-                    'start_time': stage['startTimeMillis']
-                })
-            break
-    return pipeline_data
-
-# Get the last build number
-build_info = server.get_build_info(job_name, 'lastBuild')
-build_number = build_info['number']
-recommendations = get_recommendations(build_number)
-build_data = get_build_data(job_name)
-pipeline_data = get_pipeline_data(job_name, build_number)
+# Load the build and pipeline stages data
+with open(r'D:\New folder\POC\jenkins_build_data.json', 'r') as f:
+    build_data = json.load(f)
 
 # Flask application
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # Get the latest build number and recommendations
+    latest_build = build_data[-1]
+    build_number = latest_build['number']
+    recommendations = 'Optimize resource allocation for step X.'
+
     # Generate HTML content
     html_content = f'''
     <!doctype html>
@@ -115,39 +73,4 @@ def home():
     
     html_content += '''
           </table>
-          <h2>Pipeline Stages Data for Build {build_number}</h2>
-          <table>
-            <tr>
-              <th>Stage Name</th>
-              <th>Status</th>
-              <th>Duration (milliseconds)</th>
-              <th>Start Time (milliseconds)</th>
-            </tr>
-    '''
-    if pipeline_data:
-        for stage in pipeline_data:
-            html_content += f'''
-            <tr>
-              <td>{stage['name']}</td>
-              <td>{stage['status']}</td>
-              <td>{stage['duration']}</td>
-              <td>{stage['start_time']}</td>
-            </tr>
-            '''
-    else:
-        html_content += '''
-            <tr>
-              <td colspan="4">No pipeline stages data available.</td>
-            </tr>
-        '''
-    
-    html_content += '''
-          </table>
-        </div>
-      </body>
-    </html>
-    '''
-    return render_template_string(html_content)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+          <h2>Pipeline Stages Data
