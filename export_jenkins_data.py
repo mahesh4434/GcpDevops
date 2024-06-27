2a65705cb37a8b09fcaaa1e0b0caee815fc3aa06
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-06-27 16:29:49 +0530

    Update export_jenkins_data.py

--- export_jenkins_data.py
+++ export_jenkins_data.py
@@ -16,31 +16,18 @@ job_info = server.get_job_info(job_name)
 # Get the list of builds for the job
 builds = job_info['builds']
 
-# Collect build and pipeline stage information
+# Collect build information
 data = []
 for build in builds:
     build_info = server.get_build_info(job_name, build['number'])
-    stages_data = []
-    for action in build_info['actions']:
-        if 'stages' in action:
-            stages = action['stages']
-            for stage in stages:
-                stages_data.append({
-                    'name': stage['name'],
-                    'status': stage['status'],
-                    'duration': stage['durationMillis'],
-                    'start_time': stage['startTimeMillis']
-                })
-            break
     data.append({
         'number': build['number'],
         'result': build_info['result'],
         'duration': build_info['duration'],
-        'timestamp': build_info['timestamp'],
-        'stages': stages_data
+        'timestamp': build_info['timestamp']
     })
 
-# Save build and pipeline stages data to a JSON file in D:\New folder\POC
+# Save build data to a JSON file in D:\New folder\POC
 output_file = r'D:\New folder\POC\jenkins_build_data.json'
 with open(output_file, 'w') as f:
     json.dump(data, f)
