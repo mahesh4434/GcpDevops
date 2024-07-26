73e4e4cfc1f138de7cd636c0b9f0d3f4ff41cd17
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-07-26 18:08:25 +0530

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 # Load your dataset
-data = pd.read_csv('pipeline_data.csv')  # Make sure you have this CSV file with your historical pipeline data
+data = pd.read_csv('pipeline_data.csv')
 
 # Preprocessing steps
 X = data.drop('build_success', axis=1)
@@ -22,3 +22,6 @@ pd.DataFrame(X_train).to_csv('X_train.csv', index=False)
 pd.DataFrame(X_test).to_csv('X_test.csv', index=False)
 pd.DataFrame(y_train).to_csv('y_train.csv', index=False)
 pd.DataFrame(y_test).to_csv('y_test.csv', index=False)
+
+# Save the scaler for future use
+joblib.dump(scaler, 'scaler.pkl')
