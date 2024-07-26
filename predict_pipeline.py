e589e1348ec7e9939e6930fd8856f2c8dbddd1c4
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-07-26 17:58:25 +0530

    Update predict_pipeline.py

--- predict_pipeline.py
+++ predict_pipeline.py
@@ -1,29 +1,23 @@
-import sys
 import joblib
+import sys
 import numpy as np
 
-def predict_pipeline_success(num_commits, num_tests, test_pass_rate, previous_build_result, lines_of_code_changed):
-    # Load the trained model
-    model = joblib.load('pipeline_predictor.pkl')
-    
-    # Prepare the input features
-    features = np.array([[num_commits, num_tests, test_pass_rate, previous_build_result, lines_of_code_changed]])
-    
-    # Make prediction
-    prediction = model.predict(features)
-    
-    # Output result
-    if prediction[0] == 1:
-        return 'success'
-    else:
-        return 'fail'
+# Load the model
+model = joblib.load('pipeline_predictor.pkl')
 
-if __name__ == "__main__":
-    num_commits = int(sys.argv[1])
-    num_tests = int(sys.argv[2])
-    test_pass_rate = float(sys.argv[3])
-    previous_build_result = int(sys.argv[4])
-    lines_of_code_changed = int(sys.argv[5])
-    
-    result = predict_pipeline_success(num_commits, num_tests, test_pass_rate, previous_build_result, lines_of_code_changed)
-    print(result)
+# Example features for the new pipeline run
+num_commits = int(sys.argv[1])
+num_tests = int(sys.argv[2])
+test_pass_rate = float(sys.argv[3])
+previous_build_result = int(sys.argv[4])
+lines_of_code_changed = int(sys.argv[5])
+
+# Prepare feature array
+features = np.array([[num_commits, num_tests, test_pass_rate, previous_build_result, lines_of_code_changed]])
+
+# Predict
+prediction = model.predict(features)
+if prediction[0] == 1:
+    print("The pipeline is predicted to succeed.")
+else:
+    print("The pipeline is predicted to fail.")
