1b5abd45e8b04639ad88368b308cac6320d7fe96
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-07-26 17:19:03 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,12 +2,18 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import joblib
+import os
 
-# Load preprocessed data
-X_train = pd.read_csv('D:\\New folder\\DevOpsPOC\\DevOpsGeniAiPart\\CSV files\\X_train.csv')  # Updated path
-X_test = pd.read_csv('D:\\New folder\\DevOpsPOC\\DevOpsGeniAiPart\\CSV files\\X_test.csv')  # Updated path
-y_train = pd.read_csv('D:\\New folder\\DevOpsPOC\\DevOpsGeniAiPart\\CSV files\\y_train.csv').values.ravel()  # Updated path
-y_test = pd.read_csv('D:\\New folder\\DevOpsPOC\\DevOpsGeniAiPart\\CSV files\\y_test.csv').values.ravel()  # Updated path
+# Use path from Jenkins workspace
+X_train_path = os.path.join(os.getcwd(), 'X_train.csv')
+X_test_path = os.path.join(os.getcwd(), 'X_test.csv')
+y_train_path = os.path.join(os.getcwd(), 'y_train.csv')
+y_test_path = os.path.join(os.getcwd(), 'y_test.csv')
+
+X_train = pd.read_csv(X_train_path)
+X_test = pd.read_csv(X_test_path)
+y_train = pd.read_csv(y_train_path).values.ravel()
+y_test = pd.read_csv(y_test_path).values.ravel()
 
 # Train the model
 model = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -18,5 +24,6 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Model Accuracy: {accuracy}')
 
-# Save the model
-joblib.dump(model, 'pipeline_predictor.pkl')
+# Save the model in Jenkins workspace
+model_path = os.path.join(os.getcwd(), 'pipeline_predictor.pkl')
+joblib.dump(model, model_path)
