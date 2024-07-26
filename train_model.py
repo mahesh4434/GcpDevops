21881ca34a3ef55d5bb897c3682c2e15d71990bc
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-07-26 18:08:43 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -9,6 +9,11 @@ X_test = pd.read_csv('X_test.csv')
 y_train = pd.read_csv('y_train.csv').values.ravel()
 y_test = pd.read_csv('y_test.csv').values.ravel()
 
+# Load the scaler
+scaler = joblib.load('scaler.pkl')
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
 # Train the model
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
