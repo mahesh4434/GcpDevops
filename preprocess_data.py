f5ddd3f3e571eabe945fdcfc27c6febe7f2a35f9
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-07-26 16:53:10 +0530

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 # Load your dataset
-data = pd.read_csv('pipeline_data.csv')  # Make sure you have this CSV file with your historical pipeline data
+data = pd.read_csv('D:\\New folder\\DevOpsPOC\\DevOpsGeniAiPart\\CSV files\\pipeline_data.csv')  # Updated path
 
 # Preprocessing steps
 X = data.drop('build_success', axis=1)
@@ -18,7 +18,7 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Save the preprocessed data
-pd.DataFrame(X_train).to_csv('X_train.csv', index=False)
-pd.DataFrame(X_test).to_csv('X_test.csv', index=False)
-pd.DataFrame(y_train).to_csv('y_train.csv', index=False)
-pd.DataFrame(y_test).to_csv('y_test.csv', index=False)
+pd.DataFrame(X_train).to_csv('D:\\New folder\\DevOpsPOC\\DevOpsGeniAiPart\\CSV files\\X_train.csv', index=False)  # Updated path
+pd.DataFrame(X_test).to_csv('D:\\New folder\\DevOpsPOC\\DevOpsGeniAiPart\\CSV files\\X_test.csv', index=False)  # Updated path
+pd.DataFrame(y_train).to_csv('D:\\New folder\\DevOpsPOC\\DevOpsGeniAiPart\\CSV files\\y_train.csv', index=False)  # Updated path
+pd.DataFrame(y_test).to_csv('D:\\New folder\\DevOpsPOC\\DevOpsGeniAiPart\\CSV files\\y_test.csv', index=False)  # Updated path
