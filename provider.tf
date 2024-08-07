f9c80fa8e5e4e792da7e5d9bc568eaef74c2988e
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-08-07 12:49:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,13 @@
 provider "google" {
- # credentials = file("D:/New folder/gcpdevopstest-424606-4e07e34ec952.json")
+  credentials = data.google_secret_manager_secret_version.credentials.secret_data
   project     = "devopsgkestandardproject"
   region      = "us-central1"
+}
+
+data "google_secret_manager_secret" "credentials" {
+  secret_id = "my-secret-name"  # Replace with your secret name
+}
+
+data "google_secret_manager_secret_version" "credentials" {
+  secret_id = data.google_secret_manager_secret.credentials.id
 }
